b5c27f87297b59e3c57dffeeae24eb4bd2a6dbe9
Marlen Awwad <marlenawsh3@gmail.com>
2018-01-28 15:47:54 +0200

    Created Landing Component

---
+++ ReviewApp/client/src/components/Landing.js
@@ -0,0 +1,14 @@
+import React from "react";
+
+const Landing = () => {
+  return(
+    <div style={{ textAlign: 'center' }}>
+      <h1>
+        Homeeee!
+      </h1>
+      This website is going to be flameees!
+    </div>
+  );
+};
+
+export default Landing;
