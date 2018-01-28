13518ecd8e00d99ad60f8418c30ddfedccc038f2
Marlen Awwad <marlenawsh3@gmail.com>
2018-01-28 15:19:53 +0200

    Using axios to see if the user if loggedin or loggedout on the navbar

--- ReviewApp/client/src/components/Header.js
+++ ReviewApp/client/src/components/Header.js
@@ -1,13 +1,27 @@
-import React, { Component } from 'react';
+import React, { Component } from "react";
+import { connect } from "react-redux";
 
 class Header extends Component {
-  render(){
-    return(
+
+  renderContent(){
+    switch(this.props.auth){
+      case null:
+        return 'Still deciding';
+      case false:
+        return 'Im loggedout';
+      default:
+        return 'Im loggedin';
+    }
+  }
+
+  render() {
+    console.log(this.props);
+    return (
       <nav>
         <div className="nav-wrapper">
-          <a href="#" className="left brand-logo">Logo</a>
+          <a className="left brand-logo">Logo</a>
           <ul id="nav-mobile" className="right">
-            <li><a href="#">Login With Google</a></li>
+            {this.renderContent()}
           </ul>
         </div>
       </nav>
@@ -15,4 +29,8 @@ class Header extends Component {
   }
 }
 
-export default Header;
+function mapStateToProps({ auth }){
+  return { auth: auth };  // the auth key is coming from ../reducers/index.js
+}
+
+export default connect(mapStateToProps)(Header);
--- ReviewApp/client/src/reducers/authReducer.js
+++ ReviewApp/client/src/reducers/authReducer.js
@@ -1,5 +1,10 @@
-export default function(state = {}, action) {
+import { FETCH_USER } from '../actions/types';
+
+export default function(state = null, action) {
+  // console.log(action); //this will show you the payload in the console
   switch (action.type) {
+    case FETCH_USER:
+      return action.payload || false; // That way the payload in console.log is not an empty string..sondern its a false as in the user is not logged in
     default:
       return state;
   }
