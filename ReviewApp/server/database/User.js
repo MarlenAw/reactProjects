2f937c8a0f037c50b774fbd03a19d0d1f9941910
Marlen Awwad <marlenawsh3@gmail.com>
2018-01-28 21:38:31 +0200

    Adding/updating credits
    Reference #13

--- ReviewApp/server/database/User.js
+++ ReviewApp/server/database/User.js
@@ -2,7 +2,9 @@ const mongoose = require('mongoose');
 const {Schema} = mongoose;
 
 const userSchema = new Schema({
-  googleID: String
+  googleID: String,
+  credits: { type: Number, default: 0 } //the credit starts off at 0
+
 });
 
 mongoose.model('users', userSchema);
