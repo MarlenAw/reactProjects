04d68b38f5df86d2246183568edc72c7f8a976a8
Marlen Awwad <marlenawsh3@gmail.com>
2018-01-29 16:46:37 +0200

    Routing in production mode

--- ReviewApp/server/server.js
+++ ReviewApp/server/server.js
@@ -27,6 +27,15 @@ app.use(passport.session());
 require("./routes/authenticationRoutes")(app);
 require("./routes/paymentsRoutes")(app);
 
+if(process.env.NODE_ENV === 'production'){
+  app.use(express.static('client/build'));
+
+  const path = require('path');
+  app.get('*', (req, res) => {
+    res.sendFile(path.resolve(__dirname, 'client', 'build', 'index.html'));
+  });
+}
+
 const port = process.env.PORT || 5000;
 app.listen(`${port}`, () => {
   console.log(`Express server listening on port https://localhost:${port}`);
