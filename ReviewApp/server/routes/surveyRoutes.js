c0e739ad9704df0b17b5a5f95920e6a48bbb2be0
Marlen Awwad <marlenawsh3@gmail.com>
2018-01-30 12:14:20 +0200

    Creating surveys

--- ReviewApp/server/routes/surveyRoutes.js
+++ ReviewApp/server/routes/surveyRoutes.js
@@ -1,8 +1,25 @@
+const mongoose = require("mongoose");
 const requireLogin = require("../middlewares/requireLogin");
 const requireCredits = require("../middlewares/requireCredits");
 
-module.exports = app => {
-  app.post('/api/dashboard', requireLogin, requireCredits, (req, res) => {  //check if the user loggedin first, then check if the user has credits! 
+const Survey = mongoose.model('surveys');
 
+module.exports = app => {
+  app.post('/api/dashboard', requireLogin, requireCredits, (req, res) => {  //check if the user loggedin first, then check if the user has credits!
+    const { title, subject, body, recipients } = req.body; //taking it from surveySchema
+
+    const survey = new Survey({
+      title,
+      subject,
+      body,
+      recipients: recipients.split(',').map(email => ({ email: email.trim() })), //recipients array contains a list of emails..so split to get only the emails without the comma and then do map
+      _user: req.user.id, //link the current user to the survey here..and this id = req.user.id = mongoose has generated it
+      dateSent: Date.now()  
+    });
   });
 }
+
+
+
+// .map(email => ({ email }) is exactly this :
+// .map(email => { return {email: email}});
