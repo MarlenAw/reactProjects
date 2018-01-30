28066667dfb972f0efef740469a2aff3318eb1ef
Marlen Awwad <marlenawsh3@gmail.com>
2018-01-30 19:58:41 +0200

    Setting up Mailer

--- ReviewApp/server/routes/surveyRoutes.js
+++ ReviewApp/server/routes/surveyRoutes.js
@@ -1,7 +1,8 @@
 const mongoose = require("mongoose");
 const requireLogin = require("../middlewares/requireLogin");
 const requireCredits = require("../middlewares/requireCredits");
-
+const Mailer = require("../services/Mailer");
+const surveyTemplate = require("../services/emailTemplates/surveyTemplate");
 const Survey = mongoose.model('surveys');
 
 module.exports = app => {
@@ -13,9 +14,12 @@ module.exports = app => {
       subject,
       body,
       recipients: recipients.split(',').map(email => ({ email: email.trim() })), //recipients array contains a list of emails..so split to get only the emails without the comma and then do map
-      _user: req.user.id, //link the current user to the survey here..and this id = req.user.id = mongoose has generated it
-      dateSent: Date.now()  
+      _user: req.user.id, //link the current user to the survey here..and this id = req.user.id = mongoose has generated it ..also check passport.js
+      dateSent: Date.now()
     });
+
+    const mailer = new Mailer(survey, surveyTemplate(survey));
+
   });
 }
 
