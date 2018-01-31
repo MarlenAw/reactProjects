0c6157dcecd42564a3923d2142a0bd356db63146
Marlen Awwad <marlenawsh3@gmail.com>
2018-01-31 17:20:05 +0200

    Rendering a list of surveys to the dashboard page

--- ReviewApp/client/src/components/Dashboard.js
+++ ReviewApp/client/src/components/Dashboard.js
@@ -1,10 +1,11 @@
 import React from 'react';
 import { Link } from 'react-router-dom';
+import SurveyList from './surveys/SurveyList';
 
 const Dashboard = () => {
   return (
     <div>
-      Dashboard heree!
+      <SurveyList />
 
 
       <div className="fixed-action-btn">
--- ReviewApp/server/routes/surveyRoutes.js
+++ ReviewApp/server/routes/surveyRoutes.js
@@ -10,6 +10,12 @@ const Survey = mongoose.model('surveys');
 
 module.exports = app => {
 
+  app.get('/api/dashboard', requireLogin, async (req, res) => {
+    const surveys = await Survey.find({ _user: req.user.id }).select({recipients: false});
+
+    res.send(surveys);
+  });
+
   app.get('/api/dashboard/:surveyId/:choice', (req, res) => {
     res.send('Thanks for voting!');
   });
