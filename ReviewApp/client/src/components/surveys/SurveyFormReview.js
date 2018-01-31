76e186f2df3966ab978835cc683214312a6d2cfa
Marlen Awwad <marlenawsh3@gmail.com>
2018-01-31 14:07:04 +0200

    Clearing the input fields after hitting Cancel and getting back to the page. Also creating a submitSurvey button after reviewing the survey - function not finished yet

--- ReviewApp/client/src/components/surveys/SurveyFormReview.js
+++ ReviewApp/client/src/components/surveys/SurveyFormReview.js
@@ -2,6 +2,7 @@ import _ from 'lodash';
 import React from 'react';
 import { connect } from 'react-redux';
 import formFields from './formFields';
+import * as actions from '../../actions';
 
 const SurveyFormReview = (props) =>{
 
@@ -34,6 +35,13 @@ const SurveyFormReview = (props) =>{
       <button className="yellow darken-3 btn-flat" onClick={props.onCancle}>
         Back
       </button>
+
+      <button className="green btn-flat right white-text" onClick={() => props.submitSurvey(props.formValues)}> 
+        {/* submitSurvey -> ../../actions/index  */}
+        Send Survey
+        <i className="material-icons right">email</i>
+      </button>
+
     </div>
   );
 };
@@ -45,4 +53,4 @@ function mapStateToProps(state){
   }
 }
 
-export default connect(mapStateToProps)(SurveyFormReview);
+export default connect(mapStateToProps, actions)(SurveyFormReview);
--- ReviewApp/client/src/components/surveys/SurveyNew.js
+++ ReviewApp/client/src/components/surveys/SurveyNew.js
@@ -1,4 +1,5 @@
 import React, {Component} from 'react';
+import { reduxForm } from 'redux-form';
 import SurveyForm from './SurveyForm';
 import SurveyFormReview from './SurveyFormReview';
 
@@ -22,4 +23,8 @@ class SurveyNew extends Component {
   }
 }
 
-export default SurveyNew;
+export default reduxForm({
+  form: 'surveyForm' // adding this to clear out the input fields after hitting the cancel button and clicking on add and accessing the page again..the input fields are all empty
+  // this works because we didn't include destroyOnUnmount: false
+
+})(SurveyNew);
