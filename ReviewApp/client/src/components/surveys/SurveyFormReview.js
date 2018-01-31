fec3e9bc1883f9d125ebe2d28e3671b302a22c76
Marlen Awwad <marlenawsh3@gmail.com>
2018-01-31 13:41:34 +0200

    Adding the entries to SurveyFormReview for reviewing

--- ReviewApp/client/src/components/surveys/SurveyFormReview.js
+++ ReviewApp/client/src/components/surveys/SurveyFormReview.js
@@ -1,11 +1,26 @@
+import _ from 'lodash';
 import React from 'react';
 import { connect } from 'react-redux';
+import formFields from './formFields';
 
 const SurveyFormReview = (props) =>{
+
+  const reviewFields = _.map(formFields, field => {
+    return(
+      <div key={field.name}>
+        <label>{field.label}</label>
+        <div>
+          {props.formValues[field.name]}
+        </div>
+      </div>
+    );
+  });
+
   return (
     <div>
       <h5>Please confirm your entries</h5>
 
+      {/* insted of doing this for all fields..will use map */}
       {/* <div>
         <div>
           <label>Survey Title</label>
@@ -14,6 +29,7 @@ const SurveyFormReview = (props) =>{
       </div> */}
 
 
+      {reviewFields}
 
       <button className="yellow darken-3 btn-flat" onClick={props.onCancle}>
         Back
