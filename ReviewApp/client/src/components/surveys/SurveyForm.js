5f9856f997879ffaa8e9d13250acffcf6d0238d6
Marlen Awwad <marlenawsh3@gmail.com>
2018-01-31 11:11:57 +0200

    Styling the form

--- ReviewApp/client/src/components/surveys/SurveyForm.js
+++ ReviewApp/client/src/components/surveys/SurveyForm.js
@@ -1,6 +1,7 @@
 import _ from 'lodash'; //_ is has helper functions and one of them is a math function
 import React, {Component} from 'react';
 import {reduxForm, Field} from 'redux-form'; //reduxForm is actually named reducer -> but we named it reduxForm for more clearity-check ../reducers/index.js
+import {Link} from 'react-router-dom';
 import SurveyField from './SurveyField';
 
 
@@ -25,7 +26,15 @@ class SurveyForm extends Component {
       <form onSubmit={this.props.handleSubmit(values => console.log(values))}>
         {/* <Field type="text" name="surveyTitle" component="input"/> */}
         {this.renderFields()}
-        <button type="submit">Submit</button>
+
+        <Link to="/dashboard" className="red btn-flat white-text">
+          Cancel
+        </Link>
+
+        <button type="submit" className="teal btn-flat right white-text">
+          Next
+          <i className="material-icons right">done</i>
+        </button>
       </form>
     </div>
   );
