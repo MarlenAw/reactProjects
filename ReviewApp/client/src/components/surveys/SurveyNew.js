9e4d650b4d0916b8336e30696e497d3621fe5437
Marlen Awwad <marlenawsh3@gmail.com>
2018-01-31 10:10:29 +0200

    Setting up Redux-Form and creating SurveyNew Component

---
+++ ReviewApp/client/src/components/surveys/SurveyNew.js
@@ -0,0 +1,11 @@
+import React, {Component} from 'react';
+
+class SurveyNew extends Component {
+  render() {
+    return (<div>
+      SurveyNew here!!!
+    </div>);
+  }
+}
+
+export default SurveyNew;
