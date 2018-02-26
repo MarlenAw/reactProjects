f9e8c742a476c2cdbfa054dde6b468610503f115
Marlen Awwad <marlenawsh3@gmail.com>
2018-02-26 13:13:35 +0200

    Adding a new post using redux form

--- Postypost/src/actions/index.js
+++ Postypost/src/actions/index.js
@@ -1,5 +1,5 @@
 import axios from 'axios';
-import { FETCH_POSTS } from './types';
+import { FETCH_POSTS, CREATE_POST } from './types';
 
 const ROOT_URL = 'http://reduxblog.herokuapp.com/api';
 const API_KEY = '?key=fdgfh3tf36';
@@ -7,4 +7,11 @@ const API_KEY = '?key=fdgfh3tf36';
 export const fetchPosts = () => {
   const request = axios.get(`${ROOT_URL}/posts${API_KEY}`);
   return { type: FETCH_POSTS, payload: request };
+};
+
+export const createPost = (values, callback) => {
+  const request = axios
+    .post(`${ROOT_URL}/posts${API_KEY}`, values)
+    .then(() => callback()); //adding a callback so that we can redirect to home once the user click on submit - this.props.history.push('/'); in PostsNew.js
+  return { type: CREATE_POST, payload: request };
 };
--- Postypost/src/components/PostsIndex.js
+++ Postypost/src/components/PostsIndex.js
@@ -1,6 +1,7 @@
 import _ from 'lodash';
 import React, { Component } from 'react';
 import { connect } from 'react-redux';
+import { Link } from 'react-router-dom';
 import { fetchPosts } from '../actions';
 
 class PostsIndex extends Component {
@@ -21,8 +22,13 @@ class PostsIndex extends Component {
   render() {
     return (
       <div>
+        <div className="text-xs-right">
+          <Link to="/posts/new" className="btn btn-primary">
+            Add a post
+          </Link>
+        </div>
         <h3>Posts</h3>
-        <ul>{this.renderPosts()}</ul>
+        <ul className="list-group">{this.renderPosts()}</ul>
       </div>
     );
   }
