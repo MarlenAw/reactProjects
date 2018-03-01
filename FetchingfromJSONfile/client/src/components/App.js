9dd7d66d844885b5278294a5f6a1d10333f2423e
Marlen Awwad <marlenawsh3@gmail.com>
2018-03-01 13:14:20 +0200

    Fetching data from JSON using map function

--- FetchingfromJSONfile/client/src/components/App.js
+++ FetchingfromJSONfile/client/src/components/App.js
@@ -1,7 +1,14 @@
 import React, { Component } from 'react';
+
 import resume from '../resume.json';
+import Experience from './Experience';
 
 class App extends Component {
+  renderExperiences() {
+    return resume.experiences.map((item, i) => {
+      return <Experience item={item} key={i} />;
+    });
+  }
   render() {
     return (
       <div className="wrapper">
@@ -116,69 +123,9 @@ class App extends Component {
               <i className="fa fa-briefcase" />Experiences
             </h2>
 
-            <div className="item">
-              <div className="meta">
-                <div className="upper-row">
-                  <h3 className="job-title">Lead Developer</h3>
-                  <div className="time">2015 - Present</div>
-                </div>
-                <div className="company">Startup Hubs, San Francisco</div>
-              </div>
-              <div className="details">
-                <p>
-                  Describe your role here lorem ipsum dolor sit amet,
-                  consectetuer adipiscing elit. Aenean commodo ligula eget
-                  dolor. Aenean massa. Cum sociis natoque penatibus et magnis
-                  dis parturient montes, nascetur ridiculus mus. Donec quam
-                  felis, ultricies nec, pellentesque eu, pretium quis, sem.
-                  Nulla consequat massa quis enim. Donec pede justo.
-                </p>
-                <p>
-                  Sed ut perspiciatis unde omnis iste natus error sit voluptatem
-                  accusantium doloremque laudantium, totam rem aperiam, eaque
-                  ipsa quae ab illo inventore veritatis et quasi architecto
-                  beatae vitae dicta sunt explicabo.{' '}
-                </p>
-              </div>
-            </div>
+            {this.renderExperiences()}
 
-            <div className="item">
-              <div className="meta">
-                <div className="upper-row">
-                  <h3 className="job-title">Senior Software Engineer</h3>
-                  <div className="time">2014 - 2015</div>
-                </div>
-                <div className="company">Google, London</div>
-              </div>
-              <div className="details">
-                <p>
-                  Describe your role here lorem ipsum dolor sit amet,
-                  consectetuer adipiscing elit. Aenean commodo ligula eget
-                  dolor. Aenean massa. Cum sociis natoque penatibus et magnis
-                  dis parturient montes, nascetur ridiculus mus. Donec quam
-                  felis, ultricies nec, pellentesque eu, pretium quis, sem.
-                </p>
-              </div>
-            </div>
-
-            <div className="item">
-              <div className="meta">
-                <div className="upper-row">
-                  <h3 className="job-title">UI Developer</h3>
-                  <div className="time">2012 - 2014</div>
-                </div>
-                <div className="company">Amazon, London</div>
-              </div>
-              <div className="details">
-                <p>
-                  Describe your role here lorem ipsum dolor sit amet,
-                  consectetuer adipiscing elit. Aenean commodo ligula eget
-                  dolor. Aenean massa. Cum sociis natoque penatibus et magnis
-                  dis parturient montes, nascetur ridiculus mus. Donec quam
-                  felis, ultricies nec, pellentesque eu, pretium quis, sem.
-                </p>
-              </div>
-            </div>
+            
           </section>
 
           <section className="section projects-section">
